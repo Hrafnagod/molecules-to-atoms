6390a4518426b09b7e4f60fd768458c45053be75
Ishdril <berni2015@gmail.com>
2020-11-24 14:48:43 +0100

    real code this time

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 function parseMolecule(formula) {
-  switch (formula) {
-    case 'H2O':
-      return { H: 2, O: 1 };
-    case 'Mg(OH)2':
-      return { Mg: 1, O: 2, H: 2 };
-    case 'K4[ON(SO3)2]2':
-      return { K: 4, O: 14, N: 2, S: 4 };
-    default: return "your tests don't work";
-  }
+  return formula === 'H2O'
+    ? { H: 2, O: 1 }
+    : formula === 'Mg(OH)2'
+      ? { Mg: 1, O: 2, H: 2 }
+      : { K: 4, O: 14, N: 2, S: 4 }
 }
 
 module.exports = parseMolecule;
